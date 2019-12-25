b2fa167c4cb184f1f33dcfad6e5630c36f280e19
zyxwx <858285128>
2019-12-25 17:23:02 +0800

    'markdow'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,16 +4,68 @@ module.exports = {
   description: '路漫漫其修远兮...',
   themeConfig: {
     // 你的GitHub仓库，请正确填写
-    repo: 'https://github.com/zyxloveLuck/blog-demo',
+    repo: 'https://github.com/zyxloveLuck',
     // 自定义仓库链接文字。
     repoLabel: 'My GitHub',
     nav: [
-        { text: 'Home', link: '/' },
-        { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        { text: '首页', link: '/' },
+        { text: 'FirstBlog', link: '/blog/FirstBlog.md' },
+        {
+           text: '笔记',
+           items: [
+             { text: 'Markdown', link: '/markdown/Markdown'},
+             { text: 'vue', link: '/vue/components'},
+             { text: 'js', link: '/js/js1' }
+           ]
+        }
     ],
-    sidebar: [
-      ['/', '首页'],
-      ['/blog/FirstBlog.md', '我的第一篇博客']
-    ]
+    sidebar: {
+      '/vue/': [
+        {
+          title:'前端Vue',
+          collapsable: false,
+          children:[
+            '/vue/components',
+            '/vue/test',
+          ]
+        },
+        {
+          title:'高级',
+          collapsable: true,
+          children:[
+            '/vue/test1',
+          ]
+        },
+      ],
+      '/js/': [
+        {
+          title:'前端js',
+          collapsable: false,
+          children:[
+            '/js/js1',
+            '/js/js2',
+          ]
+        },
+      ]
+    }
+    // sidebar: [
+    //   {
+    //     title: 'vue',
+    //     // path: '/blog/FirstBlog',
+    //     collapsable: false,
+    //     children: [
+    //       ['/vue/components','vue组件传值'],
+    //       ['/vue/test','vuex'],
+    //       ['/vue/test1','v-model原理']
+    //     ]
+    //   },
+    //   {
+    //     title: '博客',
+    //     collapsable: false,
+    //     children: [
+    //       ['/blog/FirstBlog','记录']
+    //     ]
+    //   }
+    // ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,15 @@
 ---
 home: true
 heroImage: /vue-logo.png
-heroText: blog-demo
-tagline: 博客示例
-actionText: 快速上手 →
+heroText: 无知小生
+tagline: 欢迎来到无知小生的博客
+actionText: 关于我
 actionLink: /
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+- title: 学习文章
+  details: 以前端为主要方向，学习编程相关知识
+- title: 文档介绍
+  details: 平时编写的工具及有趣的插件的文档
+- title: 日志日常
+  details: 平时日常及有感而发的感悟或者有趣的事件记录
 ---
